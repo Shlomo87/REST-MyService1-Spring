1954a62ff2ab17ca2696155a9f2b77ba3f3c6761
Kirill Levshin <klevshin@mail.ru>
2016-06-24 14:13:09 +0300

    'c1'

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package main.java.hello;
+
+/**
+ * Created by klevshin on 6/24/2016.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,22 @@
+package main.java.hello;
+
+/**
+ * Created by klevshin on 6/24/2016.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
